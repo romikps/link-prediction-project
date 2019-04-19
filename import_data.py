0bf95ccdda34cd56cc19b37fd7be3eac2832cc4b
Roman Priscepov <roman.priscepov@hotmail.com>
2019-04-19 15:46:18 +0200

    refactor show graph

--- import_data.py
+++ import_data.py
@@ -8,32 +8,26 @@ import time
 # https://snap.stanford.edu/data/ca-HepPh.html
 hepph_data = pd.read_csv("CA-HepPh.txt", sep='\t', header=3)
 
+num_nodes_used = 30
+tuple_edge_list = list(hepph_data.itertuples(index=False, name=None))[:num_nodes_used]
+unique_nodes = np.unique([node for edge in tuple_edge_list for node in edge])
+
+# Create graph.
 hepph_graph = nx.Graph()
-unique_from_nodes = np.unique(hepph_data['FromNodeId'])
-unique_to_nodes = np.unique(hepph_data['ToNodeId'])
-if np.array_equal(unique_from_nodes, unique_to_nodes):
-    print("From and to nodes are the same")
+hepph_graph.add_nodes_from(unique_nodes)
+hepph_graph.add_edges_from(tuple_edge_list)
 
-hepph_graph.add_nodes_from(unique_from_nodes)
-
-tuple_list = list(hepph_data.itertuples(index=False, name=None))
-hepph_graph.add_edges_from(tuple_list)
-
+# Draw graph.
 nx.draw(hepph_graph)
-plt.show()
 plt.savefig(f"graph_{int(time.time())}.png")
+plt.show()
+
+
+# nx.draw_graphviz(hepph_graph)
+# nx.write_dot(hepph_graph, f"graph_{int(time.time())}.dot")
 
 # Other datasets
 # https://snap.stanford.edu/data/ca-AstroPh.html
 # https://snap.stanford.edu/data/ca-CondMat.html
-
-example_g = nx.Graph()
-for edge in tuple_list[:50]:
-    from_n, to_n = edge
-    example_g.add_node(from_n)
-    example_g.add_node(to_n)
-    example_g.add_edge(from_n, to_n)
-    
-nx.draw(example_g)
-plt.show()    
+  
     
