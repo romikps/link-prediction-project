ab9f1c8dbd05cc2e0d3bbbabc8a96d6878ec3463
Roman Priscepov <roman.priscepov@hotmail.com>
2019-05-04 20:31:56 +0200

    refactor code

--- import_data.py
+++ import_data.py
@@ -8,32 +8,31 @@ import time
 # https://snap.stanford.edu/data/ca-HepPh.html
 hepph_data = pd.read_csv("CA-HepPh.txt", sep='\t', header=3)
 
-num_nodes_used = 30
+num_nodes_used = hepph_data.shape[0]
 tuple_edge_list = list(hepph_data.itertuples(index=False, name=None))[:num_nodes_used]
 unique_nodes = np.unique([node for edge in tuple_edge_list for node in edge])
 
 # Create graph.
-hepph_graph = nx.Graph()
-hepph_graph.add_nodes_from(unique_nodes)
-hepph_graph.add_edges_from(tuple_edge_list)
+# hepph_graph = nx.Graph()
+# hepph_graph.add_nodes_from(unique_nodes)
+# hepph_graph.add_edges_from(tuple_edge_list)
 
 # Draw graph.
 # --Separately--
-pos = nx.spring_layout(hepph_graph)
+#pos = nx.spring_layout(hepph_graph)
 #nx.draw_networkx_nodes(hepph_graph, pos, node_size=10, node_color='B')
 #nx.draw_networkx_edges(hepph_graph, pos, alpha=0.5)
 
 # --Together--
-# nx.draw(hepph_graph)
-
+#nx.draw(hepph_graph)
 #plt.savefig(f"graph_{int(time.time())}.png")
-plt.show()
-
-# nx.draw_graphviz(hepph_graph)
-# nx.write_dot(hepph_graph, f"graph_{int(time.time())}.dot")
+#plt.show()
 
 # Other datasets
 # https://snap.stanford.edu/data/ca-AstroPh.html
 # https://snap.stanford.edu/data/ca-CondMat.html
   
-    
+train_part = 0.7
+break_point = int(num_nodes_used * train_part)
+train_edges = tuple_edge_list[:break_point]   
+test_edges =  tuple_edge_list[break_point:]
--- randwalk.py
+++ randwalk.py
@@ -1,7 +1,12 @@
 import networkx as nx
 import numpy
+from import_data import train_edges, test_edges, unique_nodes as nodes
 
-G = nx.gnp_random_graph(5, 0.5)
+# G = nx.gnp_random_graph(5, 0.5)
+G = nx.Graph()
+G.add_nodes_from(nodes)
+G.add_edges_from(train_edges)
+
 A = nx.adj_matrix(G)
 A = A.todense()
 A = numpy.array(A, dtype = numpy.float64)
