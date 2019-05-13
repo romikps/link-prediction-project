f86aa14a1c4be6b967c842e24c187c59f919ef77
Roman Priscepov <roman.priscepov@hotmail.com>
2019-05-13 15:54:07 +0200

    add level features

--- com_attr.py
+++ com_attr.py
@@ -1,5 +1,19 @@
-from louvain_community_detection import best_partition
+from louvain_community_detection import best_partition, dendo
 from import_data import hepph_graph
-
-for node, com in best_partition.items():
-    hepph_graph.nodes[node]["com"] = com    
+import community
+    
+for level in range(len(dendo)):
+    level_partition = community.partition_at_level(dendo, level)
+    for node, com in level_partition.items():
+        hepph_graph.nodes[node][level] = com  
+    
+for from_node, to_node in hepph_graph.edges:
+    for level in hepph_graph.nodes[from_node]:
+        if hepph_graph.nodes[from_node][level] == hepph_graph.nodes[to_node][level]:
+            hepph_graph.edges[from_node, to_node][level] = 1
+        else:
+            hepph_graph.edges[from_node, to_node][level] = 0
+            
+        
+    
+    
