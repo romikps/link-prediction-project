c51ee5f4ffed0f2b1bef84771d0c5f216ad70373
Roman Priscepov <roman.priscepov@hotmail.com>
2019-05-18 13:08:31 +0200

    create feature matrix

--- com_attr.py
+++ com_attr.py
@@ -3,6 +3,7 @@ from import_data import hepph_graph
 import community
 from minimization_problem import goal_function_single_node, goal_function_derivative_single_node
 from scipy.optimize import minimize
+import networkx as nx
     
 for level in range(len(dendo)):
     level_partition = community.partition_at_level(dendo, level)
@@ -27,6 +28,22 @@ for from_node, to_node in hepph_graph.edges:
     adjacency_matrix = args[6]
     layer_transitions = args[7]
 '''
+feature_num = level + 1
+adjacency_matrix = nx.to_numpy_matrix(hepph_graph)
+
+feature_matrix = np.zeros((adjacency_matrix.shape[0], adjacency_matrix.shape[1], feature_num), dtype=np.float64)
+
+node_to_index = {}
+for i, node in enumerate(hepph_graph.nodes):
+    node_to_index[node] = i
+
+for from_node, to_node, features in hepph_graph.edges.data():
+    i = node_to_index[from_node]
+    j = node_to_index[to_node]
+    feature_matrix[i, j] = np.array(list(features.values()))
+    
+
+
 
 
     
