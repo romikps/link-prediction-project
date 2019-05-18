9e4f2f43536e3a1693225ca36813ba7b988a766c
Roman Priscepov <roman.priscepov@hotmail.com>
2019-05-18 12:16:07 +0200

    use only one layer

--- com_attr.py
+++ com_attr.py
@@ -1,6 +1,8 @@
 from louvain_community_detection import best_partition, dendo
 from import_data import hepph_graph
 import community
+from minimization_problem import goal_function_single_node, goal_function_derivative_single_node
+from scipy.optimize import minimize
     
 for level in range(len(dendo)):
     level_partition = community.partition_at_level(dendo, level)
@@ -15,5 +17,16 @@ for from_node, to_node in hepph_graph.edges:
             hepph_graph.edges[from_node, to_node][level] = 0
             
         
-    
+'''
+    feature_matrix = args[0]
+    alpha = args[1]
+    start_node = args[2]
+    pl = args[3]
+    pd = args[4]
+    loss_func = args[5]
+    adjacency_matrix = args[6]
+    layer_transitions = args[7]
+'''
+
+
     
--- minimization_problem.py
+++ minimization_problem.py
@@ -111,9 +111,7 @@ def goal_function_single_node(w, *args):
     loss_func = args[5]
     adjacency_matrix = args[6]
     layer_transitions = args[7]
-    edge_strengths, edge_strength_derivatives = tp.generate_multiplex_edge_strength_matrices(w, feature_matrix,
-                                                                                             adjacency_matrix,
-                                                                                             layer_transitions)
+    edge_strengths, edge_strength_derivatives = tp.generate_edge_strength_matrices(w, feature_matrix, adjacency_matrix)
     Q, dQ = tp.generate_transition_probability_matrices(edge_strengths, edge_strength_derivatives, alpha, start_node)
     p, dp = PageRank(Q, dQ)
     sum = 0
