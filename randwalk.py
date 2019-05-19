721b328827d970b433be006a6ab57a58524ad2cb
Roman Priscepov <roman.priscepov@hotmail.com>
2019-05-19 12:31:55 +0200

    refactor code to be w/o derivatives

--- randwalk.py
+++ randwalk.py
@@ -1,5 +1,5 @@
 import networkx as nx
-import numpy
+import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import normalize
 from import_data import train_edges, test_edges, unique_nodes as nodes
@@ -10,7 +10,7 @@ def draw_graph(G):
     plt.show()
 
 def get_transition_mat(adj_mat):
-    degrees = numpy.sum(adj_mat, axis=1)
+    degrees = np.sum(adj_mat, axis=1)
     trans_mat = adj_mat.copy()
     for i, degree in enumerate(degrees):
         if degree != 0:
@@ -22,8 +22,8 @@ def get_probs(arr):
     if r != n_nodes:
         raise ValueError("Number of rows should equal number of nodes")
     normalized = normalize(arr.reshape(1, -1)).reshape(-1)
-    if numpy.sum(normalized) == 0:
-        restart_node = numpy.random.randint(r)
+    if np.sum(normalized) == 0:
+        restart_node = np.random.randint(r)
         normalized[restart_node] = 1
     return normalized
 
@@ -36,12 +36,12 @@ draw_graph(G)
 
 A = nx.adj_matrix(G)
 A = A.todense()
-A = numpy.array(A, dtype = numpy.float64)
+A = np.array(A, dtype = np.float64)
 
 # the degree matrix D
-# D = numpy.diag(numpy.sum(A, axis=0))
+# D = np.diag(np.sum(A, axis=0))
 # the transition matrix T
-# T = numpy.dot(numpy.linalg.inv(D), A)
+# T = np.dot(np.linalg.inv(D), A)
 T = get_transition_mat(A)
 
 # Markovian random walk 
@@ -49,17 +49,17 @@ T = get_transition_mat(A)
 walkLength = 10
 # the state vector - the i-th component indicates the probability of being at node i
 # define the starting node
-p = numpy.zeros(n_nodes)
+p = np.zeros(n_nodes)
 p[0] = 1
 p = p.reshape(-1,1)
 visited = list()
 for k in range(walkLength):
     # evaluate the next state vector
-    p = numpy.dot(T, p)
+    p = np.dot(T, p)
     # choose the node with the highest probability as the next visited node
-    # visited.append(numpy.argmax(p))
+    # visited.append(np.argmax(p))
     try:
-        next_node = numpy.random.choice(n_nodes, p=get_probs(p))
+        next_node = np.random.choice(n_nodes, p=get_probs(p))
     except ValueError as err:
         print(err)
         print("p =", get_probs(p))
@@ -81,6 +81,7 @@ rank_sorted = sorted(accumulated.items(), reverse=True, key=lambda elem: elem[1]
 def convergence(p1, p2, epsilon=1e-12):
     return np.amax(np.abs(p1 - p2)) <= epsilon
 
+
 def page_rank(Q):
     """
     Computes the stationary probabilities vector and its derivative
@@ -100,3 +101,50 @@ def page_rank(Q):
         t1 = t1 + 1
     return p[-1]
 
+
+def generate_transition_probability_matrix(edge_strength_matrix, alpha, start_node):
+    """
+    :param edge_strength_matrix: NxN matrix containing edge strengths between nodes
+    :param alpha: restart parameter
+    :param start_node: random walk start node index
+    :return: NxN matrix of transition probabilities between nodes
+    """
+    N = edge_strength_matrix.shape[0]
+    strength_row_sums = edge_strength_matrix.sum(axis=1)  # np.apply_along_axis(math.fsum, axis = 1, arr=edge_strength_matrix)
+    transition_probability_matrix = np.empty_like(edge_strength_matrix, dtype=np.float64)
+    for i in range(N):
+        if (strength_row_sums[i] != 0):
+            transition_probability_matrix[i] = edge_strength_matrix[i] / strength_row_sums[i]
+        else:
+            transition_probability_matrix[i] = np.zeros(N)
+    transition_probability_matrix = transition_probability_matrix * (1 - alpha)
+    transition_probability_matrix[:, start_node] = transition_probability_matrix[:, start_node] + alpha
+    
+    return transition_probability_matrix
+
+
+def calculate_edge_strength(feature_vector, w):
+    """
+    Defines the edge strength function
+    :param feature_vector: vector of features
+    :param w: vector of feature weights
+    :return: The edge strength of a given edge with respect to its feature vector.
+    """
+    # return math.exp(np.dot(feature_vector, w))
+    return np.dot(feature_vector, w) + 1
+
+
+def generate_edge_strength_matrix(w, feature_vector_matrix, adjacency_matrix):
+    """
+    Generates edge strength
+    :param w: vector of feature weights
+    :param feature_vector_matrix: NxNxW matrix of edge_feature_vectors between nodes
+    :param adjacency_matrix: NxN adjacency matrix of the network
+    :return: A NxN sized matrix containing the edge strengths of node pairs
+    """
+    edge_strength_matrix = np.empty([feature_vector_matrix.shape[0], feature_vector_matrix.shape[1]], dtype=np.float64)
+    for i in range(feature_vector_matrix.shape[0]):
+        for j in range(feature_vector_matrix.shape[1]):
+            edge_strength_matrix[i][j] = calculate_edge_strength(feature_vector_matrix[i][j], w) * adjacency_matrix[i][j]
+            
+    return edge_strength_matrix
